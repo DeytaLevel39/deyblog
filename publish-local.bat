0c2fee6034bc58d2130ac0750aff33c49015a06e
James Dey <james.dey@deytalytics.com>
2020-09-15 18:48:24 +0100

    .

--- publish-local.bat
+++ publish-local.bat
@@ -1,6 +1,4 @@
 pelican-themes -U pelican-themes\bootstrap2
 del pelicanconf.py
 copy pelicanconf-localhost.py pelicanconf.py
-pelican content
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" 127.0.0.1:8000
-pelican -l --ignore-cache
+pelican -lr --ignore-cache
