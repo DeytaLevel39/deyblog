28985cba8ae25fe33e66fdaad5f4db997f821440
James Dey <james.dey@deytalytics.com>
2020-09-18 15:09:01 +0100

    .

--- pelicanconf-prod.py
+++ pelicanconf-prod.py
@@ -40,3 +40,7 @@ DISQUS_SITENAME="deyblog"
 
 MAINMENUITEMS=(('Articles','blog.html'),('Download Brochure','https://onedrive.live.com/view.aspx?resid=8541F6645D6AA323!21854&ithint=file%2cpptx&authkey=!AKyiGu7IGeJy2Bc'),('Development Projects','dev_projects.html'))
 
+PDF_PROCESSOR=True
+
+PLUGIN_PATHS=['pelican-plugins',]
+PLUGINS=['pdf',]
