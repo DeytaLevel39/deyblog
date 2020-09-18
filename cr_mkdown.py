2aec79bc61eb2b70772c5d780dd1b1c65bf42c30
James Dey <james.dey@deytalytics.com>
2020-09-18 14:39:06 +0100

    .

---
+++ cr_mkdown.py
@@ -0,0 +1,7 @@
+from md2pdf.core import md2pdf
+
+md2pdf(pdf_file_path=".",
+       md_content="content/info-hairball.md",
+       md_file_path=".",
+       css_file_path=".",
+       base_url="https://deytalytics.github.io/deyblog")
