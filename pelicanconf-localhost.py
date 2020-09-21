513fcffaf519b573154e41a56c7c5165773d9669
James Dey <james.dey@deytalytics.com>
2020-09-21 13:03:20 +0100

    .

--- pelicanconf-localhost.py
+++ pelicanconf-localhost.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = 'English'
 
+DEFAULT_DATE = 'fs'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,7 @@ PATH = 'content'
 TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = 'English'
+DEFAULT_DATE = 'fs'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
